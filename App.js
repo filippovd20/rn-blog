4f517a7ff69a468d748e67f9cd026c6f9fe99413
Dmitry Filippov <filippovd20@gmail.com>
2020-10-18 01:50:59 +0200

    128 - adding context

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { exp } from 'react-native/Libraries/Animated/src/Easing';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { exp } from "react-native/Libraries/Animated/src/Easing";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import React from "react";
+import { BlogProvider } from "./src/context/BlogContext";
 
-const navigator = createStackNavigator({
-  Index: IndexScreen
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs",
+    },
   }
-})
+);
 
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App />
-}
+  return <BlogProvider>
+      <App />
+  </BlogProvider>;
+};
