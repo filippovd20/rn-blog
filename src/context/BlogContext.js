2fe74c0f4f9a1f312d6a35fb1c3757870fd5cc99
Dmitry Filippov <filippovd20@gmail.com>
2020-10-19 23:24:27 +0200

    139 - Updating the reducer with delete

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,8 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'delete_blogpost':
+      return state.filter((blogPost)=> blogPost.id !== action.payload);
     case "add_blogpost":
       return [...state, { id: Math.floor(Math.random()*99999), title: `Blog Post #${state.length + 1}` }];
     default:
@@ -15,8 +17,14 @@ const addBlogPost = (dispatch) => {
   };
 };
 
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({type: "delete_blogpost", payload: id});
+  };
+}
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -5,7 +5,7 @@ import {Feather} from '@expo/vector-icons';
 
 
 const IndexScreen = () => {
-  const {state, addBlogPost} = useContext(BlogContext);
+  const {state, addBlogPost, deleteBlogPost} = useContext(BlogContext);
 
   return (
     <View>
@@ -16,7 +16,7 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return <View style={styles.row}>
             <Text style={styles.title}>{item.title} -  {item.id}</Text>
-            <TouchableOpacity onPress = {()=>console.log(item.id)}>
+            <TouchableOpacity onPress = {()=>deleteBlogPost(item.id)}>
               <Feather style={styles.icon} name="trash" />
             </TouchableOpacity>
           </View>
