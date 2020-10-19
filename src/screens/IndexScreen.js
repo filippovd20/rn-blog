d7602adb7a69555fdf3e7f55883b22d8d943af57
Dmitry Filippov <filippovd20@gmail.com>
2020-10-20 00:00:14 +0200

    144 - header navigation

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -39,6 +39,12 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: () => <TouchableOpacity onPress={()=>navigation.navigate('Create')}><Feather name="plus" size={30} /></TouchableOpacity>
+  };
+};
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: "row",
