35797f76a8894bece6e3d9d86e1ae546b4818ce6
Dmitry Filippov <filippovd20@gmail.com>
2020-10-21 23:20:55 +0200

    157 - Editing in a Reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,6 +4,11 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
+    case "edit_blogpost":
+      return state.map((blogPost)=> {
+        return blogPost.id === action.payload.id
+          ? action.payload : blogPost;
+      });
     case "add_blogpost":
       return [
         ...state,
