4457068fd9c593e75e83d6c98507a4208b1c410a
Dmitry Filippov <filippovd20@gmail.com>
2021-04-02 01:25:01 +0200

    adding json server and getblogposts context func

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://b81f273b8000.ngrok.io'
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from "./createDataContext";
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "get_blogposts":
+      return action.payload;
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "edit_blogpost":
@@ -22,6 +25,14 @@ const blogReducer = (state, action) => {
       return state;
   }
 };
+
+const getBlogPosts = dispatch => {
+  return async () => {
+      const response = await jsonServer.get('/blogposts')
+
+      dispatch ({ type: 'get_blogposts', payload: response.data})
+  }
+}
 
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
@@ -45,6 +56,6 @@ const editBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 );
